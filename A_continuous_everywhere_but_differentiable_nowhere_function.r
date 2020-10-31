50c53de7b00da3674626f97f48e96069297b9e1e
Arkajyoti Bhattacharjee <51816940+ArkaB-DS@users.noreply.github.com>
2020-10-31 12:11:02 +0530

    Update A_continuous_everywhere_but_differentiable_nowhere_function.r

--- A_continuous_everywhere_but_differentiable_nowhere_function.r
+++ A_continuous_everywhere_but_differentiable_nowhere_function.r
@@ -1,17 +1,15 @@
-options(warn=-1)
-f<-function(x) {
-	z=sum(1/2^(1:100)*cos(3^(1:100)*x),na.rm=T)
-	return (z) 
+options( warn = -1 )
+f <- function (x) {
+	return ( sum( 1/2^(1:10000)*cos(3^(1:10000)*x), na.rm=T ) )
 }
 
-x<-seq(-2,2,length=1000)
-y=sapply(x, function(x) f(x) )
-library(ggplot2)
-plt<-ggplot(data.frame(x=x,y=y),aes(x=x,y=y))
-plt+geom_line(col="red4")+
-	ggtitle("A continuous EVERYWHERE, but differentiable NOWHERE function")+
-	theme(
-		plot.title=element_text(color="seagreen",face="italic",hjust=0.5)
-		)
+x <- seq(-2, 2, length = 1000)
+y <- sapply(x, function(x) f(x) )
 
-	
+library(ggplot2)
+plt <- ggplot( data.frame(x=x,y=y), aes(x=x,y=y) )
+plt + geom_line( col = "red4" ) +
+	ggtitle("A continuous EVERYWHERE, but differentiable NOWHERE function") +
+	theme(
+	      plot.title = element_text( color = "seagreen" , face = "italic" , hjust = 0.5 )
+	     )
